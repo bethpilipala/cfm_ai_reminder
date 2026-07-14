d8f1b2387571d1a45e0f16dfd502fb3826143504
Beth Perkins <beth@perkcan.net>
2026-07-14 00:56:00 -0600

    Testing scripture_parser

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def main():
 
     lesson = find_current_lesson()
 
+    from scripture_parser import parse_scripture_assignment
+
+    chapters = parse_scripture_assignment(
+        lesson.scripture_assignment
+    )
+
     print()
 
     print(f"Lesson     : {lesson.lesson_number}")
@@ -14,6 +20,16 @@ def main():
     print(f"End Date   : {lesson.week.end}")
     print(f"Title      : {lesson.title}")
     print(f"Assignment : {lesson.scripture_assignment}")
+
+    print()
+
+    print("Chapters:")
+
+    if not chapters:
+        print("  No scripture chapters.")
+    else:
+        for chapter in chapters:
+            print(f"  {chapter.book} {chapter.chapter}")
 
 
 if __name__ == "__main__":
